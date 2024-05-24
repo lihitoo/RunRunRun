e2bb23611e15ce13c93ba10fff1c72b8ab005173
lihitoo <trinhtuu.05@gmail.com>
2024-05-24 18:59:48 +0700

    +Player controller

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    float halfScreeen = Screen.width / 2;
+    
     [SerializeField] private Transform playerTransform;
 
     // Update is called once per frame
@@ -15,13 +15,17 @@ public class PlayerController : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
             Move();
+            //Debug.Log(0);
         }
     }
 
     private void Move()
     {
+        float halfScreeen = Screen.width / 2;
         float xPos = (Input.mousePosition.x - halfScreeen) / halfScreeen;
-        playerTransform.localPosition += new Vector3(xPos, 1, 0);
+        playerTransform.localPosition = new Vector3(xPos*9, 2, 0);
+        Debug.Log(xPos);
+        Debug.Log(playerTransform.localPosition);
     }
 
     
